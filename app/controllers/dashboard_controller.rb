729165f1c8b58a0b57ba5d3c8710f1f846eb84f9
JasonLev <jasond.levy@gmail.com>
2014-05-28 04:50:39 -0700

    new method in controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,9 +3,10 @@ class DashboardController < ApplicationController
   def index
   end
 
-  def api
+  def nexmo_api_call
     key = ENV["NEXMO_KEY"]
     secret = ENV["NEXMO_SECRET"]
+
   end
 
 end
