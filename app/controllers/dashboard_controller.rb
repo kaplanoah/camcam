9e279002b614f9487f618b271ff542fb9337d8f7
JasonLev <jasond.levy@gmail.com>
2014-05-28 09:11:55 -0700

    add initializer and gem

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,7 @@
 class DashboardController < ApplicationController
-
 before_filter :authenticate_user!, except: :index
 
   def index
-  end
-
-  def nexmo_api_call
-    key = ENV["NEXMO_KEY"]
-    secret = ENV["NEXMO_SECRET"]
-
   end
 
 end
