57d022b6631bdcc363fd7ea8bf3537ee7615c337
paninapress <NPacifico25@gmail.com>
2014-05-28 08:17:44 -0700

    alert advanced options form works

--- app/controllers/alert_controller.rb
+++ app/controllers/alert_controller.rb
@@ -1,7 +1,21 @@
 class AlertController < ApplicationController
   
-  def new
-    @alert = Alert.new
+  def create
+    advanced_settings = params.require(:alert).permit(:phone,:email,:people,:start,:end)
+    
+    # defaults to 2 hours monitored if one is missing
+    if params[:alert][:start] == "" && params[:alert][:end] == ""
+      advanced_settings[:allday] = true
+    elsif params[:alert][:start] == "" && params[:alert][:end] != nil
+      advanced_settings[:start] = Time.parse(params[:alert][:end]) - 2.hours
+    elsif params[:alert][:start] != nil && params[:alert][:end] == ""
+      advanced_settings[:end] = Time.parse(params[:alert][:start]) + 2.hours
+    else
+      advanced_settings[:allday] = false
+    end
+    
+    Alert.create(advanced_settings)
+    redirect_to dashboard_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
 
   root to: 'dashboard#index'
 
+  get '/dashboard', to: 'dashboard#show'
+  
+  post '/alert', to: 'alert#create', as: 'alert'
+
   post '/camera/alert', to: 'camera#send_alert'
 end
