814f7f55795e7cb2ce770654776e0a3a7ca9beec
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-28 11:10:21 -0700

    final fallback commit before presentation - added url to text message

--- app/controllers/camera_controller.rb
+++ app/controllers/camera_controller.rb
@@ -6,7 +6,7 @@ require 'nexmo'
 
     from = '16316141575'
     to = '12023227067'
-    message = 'More than one person! Visit '
+    message = 'More than one person! Visit https://login.eagleeyenetworks.com/hist/index.html?id=100b7d7c&ts=20140528164845.685'
     response = NEXMO.send_message({:to => to, :from => from, :text => message})
     # url = 'https://rest.nexmo.com/sms/json?api_key=' + key + '&api_secret=' + secret + '&from=' + from + '&to=' + to + '&text=' + message
     # response = open(url).read
