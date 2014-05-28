243decfcb5a25e7bddcecc0629a8b99e390605ac
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-28 09:33:19 -0700

    changed dotenv gem to dotenv-rails

--- app/controllers/camera_controller.rb
+++ app/controllers/camera_controller.rb
@@ -2,6 +2,10 @@ class CameraController < ApplicationController
 
   def send_alert
     puts "MORE THAN ONE BOX"
+
+    respond_to do |f|
+      f.json { render :json => { :status => 200 } }
+    end
   end
 
 end
