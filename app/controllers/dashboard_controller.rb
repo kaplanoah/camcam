b5bca788032046bb22efdda39a772b6ff2172eb7
paninapress <NPacifico25@gmail.com>
2014-05-28 12:17:08 -0700

    a few touch ups

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,9 @@ class DashboardController < ApplicationController
 before_filter :authenticate_user!, except: :index
 
   def index
+    if signed_in?
+      redirect_to dashboard_path
+    end
   end
 
   def show
